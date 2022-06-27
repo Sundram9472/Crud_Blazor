424b422f166b7d7248cb358fb7033191d2ee0fc7
Sundram <sundramtpvc@gmail.com>
2022-06-27 12:52:26 +0530

    Validation

--- Crud_Blazor/Data/ClgStudentDetails.cs
+++ Crud_Blazor/Data/ClgStudentDetails.cs
@@ -10,11 +10,28 @@ namespace Crud_Blazor.Data
     {
         [Key]
         public int Id { get; set; }
+
+        [MinLength(2)]
+        [Required(ErrorMessage = "FirstName is mandatory")]
         public string FirstName { get; set; }
+
+        [MinLength(2)]
+        [Required(ErrorMessage = "LastName is mandatory")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Gender is mandatory")]
         public string Gender { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Gmail is mandatory")]
         public string Gmail { get; set; }
+
+        [MinLength(2)]
+        [Required(ErrorMessage = "Programming Language is mandatory")]
         public string ProgrammingLanguage { get; set; }
+
+        [MinLength(2)]
+        [Required(ErrorMessage = "Adress is mandatory")]
         public string City { get; set; }
         
     }
