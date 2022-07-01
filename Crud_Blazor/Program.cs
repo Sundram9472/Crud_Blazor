1c50b1ddd55e29925284bfbaaf62b048fe7b7818
Sundram <sundramtpvc@gmail.com>
2022-07-01 18:05:17 +0530

    .net 6 MIgration

--- Crud_Blazor/Program.cs
+++ Crud_Blazor/Program.cs
@@ -1,5 +1,10 @@
+using Crud_Blazor.Data;
+using Crud_Blazor.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,20 +12,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Crud_Blazor
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+// Add services to the container.
+services.AddRazorPages();
+services.AddServerSideBlazor();
+services.AddScoped<ClgStudentDetailsService>();
+services.AddAuthentication("Identity.Application").AddCookie();
+services.AddScoped<IFileUpload, FileUpload>();
+#region Connection String
+services.AddDbContext<AppDBContext>(item => item.UseSqlServer(builder.Configuration.GetConnectionString("BlazorDBContext")));
+#endregion
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapBlazorHub();
+    endpoints.MapDefaultControllerRoute();
+    endpoints.MapFallbackToPage("/_Host");
+});
+
+app.Run();
